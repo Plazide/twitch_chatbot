4566a7c1c3edbb303b67538426b7c66bd52bac63
20641118 <callehj@hotmail.se>
2019-03-09 18:01:13 +0100

    Remove data

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const request = require("request");
+const tmi = require("tmi.js");
+const WebSocket = require("ws");
+const mongoose = require("mongoose");
+
+const conf = require("./data/config.json");
+require("./mongoConnect");
+
+function connect(){
+
+}
