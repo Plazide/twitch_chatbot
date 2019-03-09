6c330601b2800babe7645776a6eb50700bee8d8e
20641118 <callehj@hotmail.se>
2019-03-09 22:15:16 +0100

    Add client

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const request = require("request");
 const tmi = require("tmi.js");
 const WebSocket = require("ws");
 const mongoose = require("mongoose");
-
-const conf = require("./data/config.json");
+const options = require("./options");
 require("./mongoConnect");
 
-function connect(){
+const opts = {
+	identity: {
+		username: options.twitch.bot_name,
+		password: options.twitch.bot_token
+	},
+	connection: {
+		reconnect: true
+	},
+	channels: ["iamstreaming"]
+};
 
-}
+const client = new tmi.client(opts);
+
+client.on("connected", (d) => {
+	console.log("Connected to chat server!");
+});
+
+client.on("chat", (channel, userstate, message, self) => {
+	const user = userstate["display-name"];
+
+	console.log(`${user} sent a message in ${channel}'s channel:\n${message}`);
+});
+
+client.connect();	
