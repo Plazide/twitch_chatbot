c23a06a5c35a0bae4fa24e21c0314056b394a5d7
20641118 <callehj@hotmail.se>
2019-03-24 16:32:18 +0100

    Add User model for mongoose

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	username: String,
+	display_name: String,
+	profile_img: String,
+	sess_id: String,
+	access_token: String,
+	refresh_token: String,
+	created: Date,
+	last_login: Date,
+	playlist: [
+		{
+			title: String,
+			url: String,
+			id: String,
+			duration: Number,
+			user: String,
+			added: {type: Date, default: Date.now}
+		}
+	]
+});
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
