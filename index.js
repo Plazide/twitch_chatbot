573e3688abd84e8fc603a8ad4e751aba4cf851cf
20641118 <callehj@hotmail.se>
2019-03-25 20:34:50 +0100

    Move preperation functions to pre its own file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tmi = require("tmi.js");
-const TwitchApi = require("node-twitch");
 const options = require("./options");
 const commands = require("./lib/commands");
+const pre = require("./lib/pre");
 require("./mongoConnect");
 
 const opts = {
@@ -16,11 +16,6 @@ const opts = {
 };
 
 const client = new tmi.Client(opts);
-const api = new TwitchApi({
-	client_id: options.twitch.client_id,
-	client_secret: options.twitch.client_secret,
-	isApp: true
-});
 
 client.on("connected", (d) => {
 	console.log("Connected to chat server!");
@@ -29,7 +24,7 @@ client.on("connected", (d) => {
 client.on("chat", async (channel, userstate, message, self) => {
 	if(self) return;
 
-	const perms = await getPerms(userstate, channel.substring(1));
+	const perms = await pre.getPerms(userstate, channel.substring(1));
 	const args = {
 		message,
 		userstate,
@@ -41,99 +36,6 @@ client.on("chat", async (channel, userstate, message, self) => {
 	runCommand(args);
 });
 
-async function getPerms(userstate, channel){
-	const perms = {
-		broadcaster: userstate.badges.broadcaster ? true : false,
-		mod: userstate.badges.moderator ? true : false,
-		sub: userstate.badges.subscriber ? true : false,
-		follower: await isFollower(userstate["user-id"], channel),
-		vip: userstate.badges.vip ? true : false
-	}
-
-	return perms;
-}
-
-async function isFollower(user, channel){
-	let isFollower = null;
-
-	const result = await api.getUsers(channel);
-	const channel_id = result.data[0].id;
-	
-	const options = {
-		from_id: user,
-		to_id: channel_id
-	}
-
-	const follows = await api.getFollows(options);
-
-	if(follows.data.length > 0)
-		isFollower = true;
-	else
-		isFollower = false;
-
-	return isFollower;
-}
-
-function getRequiredRank(ranks, cmd){
-	let requiredRank = commands.known.filter( command => {
-		if(command.name === cmd){
-			return true;
-		}
-	});
-	requiredRank = ranks[requiredRank[0].perm];
-
-	return requiredRank;
-}
-
-function hasPermission(cmd, perms){
-	let rank = 0;
-	const ranks = {
-		all: 0,
-		vip: 1,
-		follower: 2,
-		sub: 3,
-		mod: 4,
-		broadcaster: 5
-	}
-	const requiredRank = getRequiredRank(ranks, cmd)
-	
-	Object.keys(perms).forEach( key => {
-		const hasPerm = perms[key];
-		if(hasPerm){
-			const thisRank = ranks[key];
-			if(thisRank > rank)
-				rank = thisRank;
-		}	
-	});
-
-	if(rank >= requiredRank)
-		return true;
-	else	
-		return false;
-}
-
-function isKnownCommand(cmd){
-	let result = false;
-
-	commands.known.forEach( (known) => {
-		if(known.name === cmd)
-			result = true;
-	});
-
-	return result;
-}
-
-function findCmdFunc(cmd){
-	let res;
-
-	for(let item of commands.known){
-		if(item.name === cmd)
-			res = item.func;
-	}
-
-	return res;
-}
-
 function runCommand(args){
 	const message = args.message;
 	const command = args.message.split(" ")[0].substring(1).trim();
@@ -144,16 +46,16 @@ function runCommand(args){
 
 	// Cancel execution of function if the command is invalid in some way.
 	if(message[0] !== "!") return;
-	if(!isKnownCommand(command)){
+	if(!pre.isKnownCommand(command)){
 		client.say(channel, `The command !${command} does not exist. Type !help for a list of available commands.`);
 		return;
 	};
-	if(!hasPermission(command, perms)){
+	if(!pre.hasPermission(command, perms)){
 		client.say(channel, `@${chatter}, you do not have permission to use that command!`);
 		return;
 	}
 
-	const cmd = findCmdFunc(command);
+	const cmd = pre.findCmdFunc(command);
 	const cmdArgs = message.split(" ").slice(1);
 	const params = {
 		client,
