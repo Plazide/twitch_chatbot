7f64ec25ebf755a6434b8920c7dd9103b9d00051
20641118 <callehj@hotmail.se>
2019-04-11 17:27:22 +0200

    Switch to strictly using environment variables to compose option object

--- models/options.js
+++ models/options.js
@@ -1,7 +1,22 @@
 require("dotenv").config();
-const config = require("../data/config.json");
-const env = process.env.NODE_ENV || "development";
+//const config = require("../data/config.json");
+//const env = process.env.NODE_ENV || "development";
 
-const options = config[env];
+//const options = config[env];
+
+const options = {
+	"twitch": {
+		"bot_name": process.env.BOT_NAME,
+		"bot_token": process.env.BOT_TOKEN,
+		"client_id": process.env.CLIENT_ID,
+		"client_secret": process.env.CLIENT_SECRET,
+	},
+	"mongo": {
+		"uri": process.env.MONGODB_URI
+	},
+	"youtube": {
+		"api_key": process.env.Y_API_KEY
+	}
+}
 
 module.exports = options;
